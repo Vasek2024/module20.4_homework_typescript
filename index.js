cd9130c54d1ee188b68108bc5578a9fba332a77f
Vasek2024 <p.f1@yandex.ru>
2025-04-26 19:01:09 +0700

    правка кода под typescript

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+export{}
+
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
+// Добавляет крестик каждому itom
+const myNodelist:HTMLCollectionOf<Element> = document.getElementsByTagName("LI");
+let i;
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  const span:HTMLElement = document.createElement("SPAN");
+  const txt:Text = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
+// Клик по иконке (плюсик)
+const close:HTMLCollectionOf<Element> = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function () {
@@ -20,7 +24,7 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
+const list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
@@ -29,9 +33,9 @@ list.addEventListener('click', function (ev) {
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("myInput").value;
+  const t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
@@ -40,16 +44,95 @@ function newElement() {
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-      var div = this.parentElement;
+      const div = this.parentElement;
       div.style.display = "none";
     }
   }
 }
+
+
+// ----------------------------------------
+// export {};
+
+// // Create a "close" button and append it to each list item
+// const myNodelist: HTMLCollectionOf<Element>  = document.getElementsByTagName("LI");
+// let i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   const span:HTMLElement = document.createElement("SPAN");
+//   const txt:Text = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+
+// // Click on a close button to hide the current list item
+// let closeButtons: HTMLCollectionOf<Element> = document.getElementsByClassName("close");
+// for (i = 0; i < closeButtons.length; i++) {
+//   let closeButton:Element = closeButtons[i];
+//   closeButton.addEventListener("click", ()=>{
+//     const li = closeButton.parentElement;
+//     if (!!li) {
+//       li.style.display = "none";
+//     }
+//   });
+// }
+
+// // Add a "checked" symbol when clicking on a list item
+// const list: HTMLUListElement | null = document.querySelector('ul');
+// if (list) {
+//   list.addEventListener('click', function (ev) {
+//     let target:HTMLElement = <HTMLElement>ev.target;
+//     if (target) {
+//       if (target.tagName === 'LI') {
+//         target.classList.toggle('checked');
+//       }
+//     }
+//   }, false);
+// }
+
+
+// // Create a new list item when clicking on the "Add" button
+// function newElement():void {
+//   const li:HTMLLIElement = document.createElement("li");
+//   const input: HTMLInputElement = <HTMLInputElement>document.getElementById("myInput");
+//   if (!input) return;
+//   const inputValue = input.value;
+//   const textNode: Text = document.createTextNode(inputValue);
+//   li.appendChild(textNode);
+
+//   if (inputValue === '') {
+//     alert("You must write something!");
+//   } else {
+//     const ul = document.getElementById("myUL");
+//     if (ul) {
+//       ul.appendChild(li);
+//     }
+//   }
+//   input.value = "";
+
+//   const span = document.createElement("SPAN");
+//   const txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   li.appendChild(span);
+
+//   span.addEventListener("click", ()=>{
+//     const li = span.parentElement;
+//     if (!!li) {
+//       li.style.display = "none";
+//     }
+//   });
+// }
+
+// const addBtn: HTMLElement | null = document.getElementById("addBtn");
+// addBtn?.addEventListener("click", ()=> {
+//   newElement();
+// });
